ec59fbb166ac45ba5842547dfd162a395a4308ac
Eric Scherfling <scherflinge@wit.edu>
2020-07-15 16:15:49 -0400

    adding code

---
+++ tunefinder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TunefinderConfig(AppConfig):
+    name = 'tunefinder'
---
+++ tunefinder/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponseRedirect
+from .upload_file import handle_uploaded_file
+from .forms import UploadFileForm
+from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    context = {}
+    return render(request, 'tunefinder/homepage.html', context)
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'tunefinder/upload.html', {'form': form})
